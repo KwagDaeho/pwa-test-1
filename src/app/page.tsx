76ff6ea62eff36df6a18d833b0a0e8e398abe653
daeho-gn <daeho.kwag@genesisnest.com>
2024-11-05 15:21:09 +0900

    fix: fix manifest

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,19 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <div>Hi! My Name is 9DuCK3!</div>
+        <h1>93DuCK - PWA APP </h1>
+        <div>
+          {/* <a href="https://open.kakao.com/o/sFBLKRZe" target="_blank"> */}
+          카카오 오픈채팅 연결 [새 창이 열려요!]
+          {/* </a> */}
+        </div>
+        <div>
+          {/* <a
+            href="https://www.notion.so/darby-daeho/f16abea6cc5c4791998b6a062173da93?pvs=4"
+            target="_blank"> */}
+          노션 연결 [새 창이 열려요!]
+          {/* </a> */}
+        </div>
         <InstallPrompt />
       </main>
       <footer className={styles.footer}></footer>
