10f5759e440bb1c3dd3884d802cff3d0e9a9946e
daeho-gn <daeho.kwag@genesisnest.com>
2024-11-27 14:28:34 +0900

    fix: 로직 다이나믹 임포트

--- src/app/Game/CrossingBridge/page.tsx
+++ src/app/Game/CrossingBridge/page.tsx
@@ -1,9 +1,20 @@
 "use client";
 
 import "./style.css";
-import "./logic.js";
+import { useEffect } from "react";
 
 export default function CrossingBridge() {
+  useEffect(() => {
+    // 클라이언트에서만 실행되는 로직
+    import("./logic.js")
+      .then(() => {
+        // 로딩이 끝난 후, 해당 모듈을 사용할 수 있습니다.
+        console.log("logic.js가 로드되었습니다!");
+      })
+      .catch((error) => {
+        console.error("logic.js 로드 중 오류 발생:", error);
+      });
+  }, []);
   return (
     <>
       <div className="container">
@@ -13,7 +24,6 @@ export default function CrossingBridge() {
         <div id="perfect">DOUBLE SCORE</div>
         <button id="restart">RESTART</button>
       </div>
-      <style></style>
     </>
   );
 }
