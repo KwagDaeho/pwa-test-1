6f26ed0b8401fdc6983954a5fbba2f061db6fe09
daeho-gn <daeho.kwag@genesisnest.com>
2024-12-06 11:10:10 +0900

    feat: 리더보드 스타일 추가

--- src/app/Game/LeaderBoard/Santa/page.tsx
+++ src/app/Game/LeaderBoard/Santa/page.tsx
@@ -23,7 +23,7 @@ export default function LeaderBoard() {
           alignItems: "center",
           gap: "24px",
         }}>
-        <h2>[ Game : Santa ]</h2>
+        <h2>[ Santa ]</h2>
         {loading && <p>Loading...</p>}
         {error && <p style={{ color: "red" }}>{error}</p>}
         {data && (
@@ -43,9 +43,36 @@ export default function LeaderBoard() {
                         justifyContent: "space-around",
                         gap: "20px",
                         borderBottom: `${
-                          index < 3 ? "2px solid #f90" : "2px solid #ccc"
+                          index == 0
+                            ? "4px solid #ffd700"
+                            : index == 1
+                            ? "3px solid #BEBEBE"
+                            : index == 2
+                            ? "3px solid #CD7F32"
+                            : "2px solid #999"
                         }`,
-                        padding: "4px 24px",
+                        padding: `${
+                          index == 0
+                            ? "30px 24px 18px 60px"
+                            : index < 3
+                            ? "18px 24px 12px 48px"
+                            : "4px 24px"
+                        }`,
+                        backgroundImage: `url(${
+                          index == 0
+                            ? "/image/rank-1.png"
+                            : index == 1
+                            ? "/image/rank-2.png"
+                            : index == 2
+                            ? "/image/rank-3.png"
+                            : ""
+                        })`,
+                        backgroundRepeat: "no-repeat",
+                        backgroundSize: `${index == 0 ? "60px" : "40px"}`,
+                        backgroundPosition: "0 50%",
+                        fontSize: `${
+                          index == 0 ? "1.5em" : index < 3 ? "1.2em" : "1em"
+                        }`,
                       }}>
                       <span style={{ width: "240px" }}>
                         <b>{item.properties.Name.title[0].text.content}</b> 님{" "}
--- src/app/Game/LeaderBoard/page.tsx
+++ src/app/Game/LeaderBoard/page.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import Link from "next/link";
-import "./style.css";
 
 export default function LeaderBoard() {
   return (
