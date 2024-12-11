3d94ef7e9919cd99aa07d6cd926556347285790c
daeho-gn <daeho.kwag@genesisnest.com>
2024-12-11 21:11:48 +0900

    fix: 구글 로그인 방식 변경

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,4 +19,14 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-export const auth = getAuth(firebaseApp);
+const auth = getAuth();
+const provider = new GoogleAuthProvider();
+
+signInWithPopup(auth, provider)
+  .then((result) => {
+    const user = result.user; // 로그인된 사용자 정보
+    console.log("User:", user);
+  })
+  .catch((error) => {
+    console.error("Error during login:", error.message);
+  });
