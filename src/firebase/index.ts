d6b5eed684fef2440644e1f9b32c8e581172973d
daeho-gn <daeho.kwag@genesisnest.com>
2024-12-12 10:12:36 +0900

    test: 구글 로그인함수 재변경 테스트

--- src/firebase/index.ts
+++ src/firebase/index.ts
@@ -1,6 +1,6 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth";
+import { getAuth } from "firebase/auth";
 
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
@@ -19,14 +19,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 export const firebaseApp = initializeApp(firebaseConfig);
-export const auth = getAuth();
-const provider = new GoogleAuthProvider();
-
-signInWithPopup(auth, provider)
-  .then((result) => {
-    const user = result.user; // 로그인된 사용자 정보
-    console.log("User:", user);
-  })
-  .catch((error) => {
-    console.error("Error during login:", error.message);
-  });
+export const auth = getAuth(firebaseApp);
