14fc64d6a36c34932af95502d4f7d24dbc6f20b2
daeho-gn <daeho.kwag@genesisnest.com>
2024-12-13 14:06:00 +0900

    feat: 구글로그인 관련 텍스트 추가

--- src/app/Game/page.tsx
+++ src/app/Game/page.tsx
@@ -18,7 +18,7 @@ export default function Game() {
       <h1>
         PWA Games
         <br />
-        by Daeho Kwag{" "}
+        by Daeho Kwag <br />
       </h1>
       <h2>== Game List ==</h2>
       <ul id="game_wrap">
@@ -51,6 +51,19 @@ export default function Game() {
           </button>
         </li>
       </ul>
+      <div
+        style={{
+          marginTop: "20px",
+          padding: "24px",
+        }}>
+        <p>
+          구글 로그인을 하시면, Set/Santa 게임 종료시
+          <br />
+          점수가 구글 닉네임으로 자동 등록됩니다.
+          <br />
+          나머지 2개 게임은 리팩토링이 아직... ㅎㅎ
+        </p>
+      </div>
     </div>
   );
 }
